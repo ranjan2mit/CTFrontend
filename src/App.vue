9428e35915e2c6fa4a24680683767939889c84a0
Vikash Ranjan <vikash.ranjan@firstcry.com>
2021-06-02 08:26:34 +0530

    vue frontend curd opration

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+  <Opration/>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Opration from './components/Opration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Opration
   }
 }
 </script>
